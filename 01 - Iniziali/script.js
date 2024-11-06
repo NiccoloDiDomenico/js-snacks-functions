84ed07a1d421526d9356438d46e8efe3565422f1
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-06 19:17:28 +0100

    feat: complete exercise 01

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,46 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+/**
+ * Definisce un arrey con le iniziali di ogni parola dell'arrey fornito
+ * @param {string} arrayToCheck
+ * @returns {string}
+ */
+function initialsNames(arrayToCheck) {
+
+    // Creo nuovo arrey vuoto
+    const result = [];
+
+    // Creo un ciclo per selezionare ogni elemento dell'arrey da controllare
+    for (let i = 0; i < names.length; i++) {
+        const currentName = names[i];
+        console.log(currentName);
+
+        // Creo una variabile vuota alla quale aggiongerò ogni iniziale di ogni nome
+        let firstLetter;
+
+        // Creo una variabile che splitta ogni lettera dell'elemento selezionato nel ciclo 
+        let separatedNameArrey = currentName.split("")
+        console.log(separatedNameArrey);
+
+        // Creo un sottociclo che seleziona tutte le lettere della variabile "separetedNameArrey"
+        for (let i = 0; i < separatedNameArrey.length; i++) {
+            firstLetter = separatedNameArrey[0]
+            console.log(firstLetter);
+        }
+        
+        // Seleziono solo gli elementi con indice [0] (ossia la prima lettera del nome) e li pusho nell'arrey vuoto
+        result.push(firstLetter)
+    }
+    
+    console.log(firstNameLetter);
+    return result
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
+initialsNames(names);
+console.log(initialsNames(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
