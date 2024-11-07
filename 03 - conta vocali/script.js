1a1058a494b1f8552c2d064193d4d2893d0862e7
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-07 10:35:42 +0100

    feat: complete exercise 03

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,34 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * Description
+ * @param {string} wordToCheck
+ * @returns {string}
+ */
+function vowelCount(wordToCheck) {
+
+    const vocal = ["a", "e", "i", "o", "u"];
+    console.log(vocal);
+    
+    let count = 0;
+
+    for (let i = 0; i < wordToCheck.length; i++) {
+        const currentItem = wordToCheck[i];
+        console.log(currentItem);
+
+        if (vocal.includes(currentItem.toLowerCase()))
+        count++
+    }
+    
+    console.log(count);
+    return count
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const vocaleCount = vowelCount(word)
+console.log(vocaleCount);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
