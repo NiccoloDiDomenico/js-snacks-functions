444f123b9523c248a6d5c62cfe8b363ed7f3c07d
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-07 10:49:13 +0100

    feat: complete bonus exercise 05

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -7,12 +7,35 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Definisci un saluto con dei parametri
+ * @param {string} nameToCheck
+ * @returns {string} 
+ */
+function greetings(nameToCheck) {
+   
+    const now = new Date();
+    const hours = now.getHours();
+    console.log(hours);
+
+    let message;
+    if (hours >= 0 && hours < 13) {
+        message = `Buongiorno`
+    } else if (hours >= 13 && hours <= 17) {
+        message = `Buon pomeriggio`
+    } else if (hours > 17 && hours > 0) {
+        message = `Buonasera`
+    }
+    console.log(message);
+
+    return `${message} ${nameToCheck}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+    const hello = greetings(name)
+    console.log(hello);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
